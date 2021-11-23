ccc723d015e7306a536f38ce7d72ef407fef68f3
Kevin Hong <kevin880208@gmail.com>
2021-11-24 01:31:47 +0800

    add MST

--- MST_prim/MST_prim.cpp
+++ MST_prim/MST_prim.cpp
@@ -2,6 +2,74 @@
 #include <vector>
 #include <iomanip>
 
+const int Max_Weight = 1000;
+
+class GraphMST{
+private:
+  int num_vertex;
+  std::vector< std::vector<int> > AdjMatrix;
+public:
+  GraphMST():num_vertex(0){};
+  GraphMST(int n):num_vertex(n){
+    AdjMatrix.resize(num_vertex);
+    for (int i=0; i < num_vertex; i++){
+      AdjMatrix[i].resize(num_vertex);
+    }
+  }
+  void AddEdge(int from, int to, int weight);
+
+  void PrimMST(int Start = 0);
+  friend int MinKeyExtract(int *key, bool *visited, int size);
+};
+
+int MinKeyExtract(int *key, bool *visited, int size){
+  
+  int min = Max_Weight, min_idx = 0;
+  for (int i = 0; i < size; i++){
+    if (visited[i] == false && key[i] < min) {
+      min = key[i];
+      min_idx = i;
+    }
+  }
+  return min_idx;
+}
+
+void GraphMST::PrimMST(int Start){
+  
+  int key[num_vertex], predecessor[num_vertex];
+  bool visited[num_vertex];
+
+  for (int i = 0; i < num_vertex; i++) {
+    key[i] = Max_Weight;
+    predecessor[i] = -1;
+    visited[i] = false;
+  }
+
+  key[Start] = 0;
+  for (int i = 0; i < num_vertex; i++){
+    int u = MinKeyExtract(key, visited, num_vertex);
+    visited[u] = true;
+    for (int j = 0; j < num_vertex; j++){
+      if (visited[j] == false && AdjMatrix[u][j]!=0 && AdjMatrix[u][j]<key[j]) {
+        predecessor[j] = u;
+	key[j] = AdjMatrix[u][j];
+      }
+    }
+  }
+  std::cout << std::setw(3) << "v1" << " - " << std::setw(3) << "v2"<< " : weight\n";
+  int i = (Start+1)%num_vertex;
+  while (i != Start) {
+        std::cout << std::setw(3) << predecessor[i] << " - " << std::setw(3) << i
+                  << " : " << std::setw(4) << AdjMatrix[predecessor[i]][i] <<"\n";
+        i = (++i)%num_vertex;       // 到了6之後, 6+1 = 7, error:bad_access, 透過mod把7喬回0
+  }
+}
+
+
+void GraphMST::AddEdge(int from, int to, int weight){
+  AdjMatrix[from][to] = weight;
+}
+
 int main(){
 
     GraphMST g6(7);
